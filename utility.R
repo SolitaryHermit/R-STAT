bbb1d7610f71939fb80644a32974e4852fb1b48a
SolitaryHermit <47015800+SolitaryHermit@users.noreply.github.com>
2020-02-13 13:01:47 -0500

    Update utility.R

--- utility.R
+++ utility.R
@@ -4,17 +4,17 @@
 # ARGUMENT: n -- decimal number
 #------------------------------------------------------------
 bindecomp <- function(n) {
-  if (n <= 0) {
-    warning("The number should be positive!")
-    return()
-  }
-  bin <- rep(0, as.integer(log2(n)) + 1)
-  while (n > 0) {
-    index <- as.integer(log2(n))
-    bin[index + 1] <- 1
-    n <- n - 2^index
-  }
-  return(bin)
+    if (n <= 0) {
+        warning("The number should be positive!")
+        return()
+    }
+    bin <- rep(0, as.integer(log2(n)) + 1)
+    while (n > 0) {
+        index <- as.integer(log2(n))
+        bin[index + 1] <- 1
+        n <- n - 2^index
+    }
+    return(bin)
 }
 
 #------------------------------------------------------------
@@ -23,18 +23,18 @@ bindecomp <- function(n) {
 # ARGUMENT: M -- matrix; n -- exponent
 #------------------------------------------------------------
 matpow <- function(M, n) {
-  # Check whether M is a square matrix 
-  if (nrow(M) != ncol(M)) {
-    warning("The size of input matrix is invalid!")
-    return()
-  } 
-  bin <- bindecomp(n) # Obtain the binary expansion of n
-  power <- diag(1, nrow(M), ncol(M))  # Initialize power as identity matrix
-  for (i in 1:length(bin)) {
-    if (bin[i] == 1) {
-      power <- power %*% M
+    # Check whether M is a square matrix 
+    if (nrow(M) != ncol(M)) {
+        warning("The size of input matrix is invalid!")
+        return()
+    } 
+    bin <- bindecomp(n) # Obtain the binary expansion of n
+    power <- diag(1, nrow(M), ncol(M))  # Initialize power as identity matrix
+    for (i in 1:length(bin)) {
+        if (bin[i] == 1) {
+          power <- power %*% M
+        }
+        M = M %*% M
     }
-    M = M %*% M
-  }
-  return(power)
+    return(power)
 }
