5ac7652a64dee7df5fb38cd32bb4d5f60a07c1da
SolitaryHermit <47015800+SolitaryHermit@users.noreply.github.com>
2020-02-13 14:02:48 -0500

    Update HMM-sim.R

--- HMM-sim.R
+++ HMM-sim.R
@@ -14,17 +14,23 @@ rownames(roll_prob) <- dice
 colnames(roll_prob) <- number
 
 # Implementation of HMM Model
+# n: The number of times of simulation
+# init_prob: The initial probability
+# trans_mat: The transition matrix P
+# outcome_mat: The outcome matrix E
+# state_label: The label of states, e.g., {"H","T"}
+# outcome_label: The label of outcomes, e.g., {1,2,3,4,5,6}
 hmm <- function(n, init_prob, trans_mat, outcome_mat, state_label, outcome_label) {
     # Set up results
     states <- rep(0, n)
     outcomes <- rep(0, n)
     # Initialize the states
     states[1] <- state_label[sample(1:length(state_label), 1, prob=init_prob)]
-    outcomes[1] <- outcome_label[sample(1:length(outcome_label), 1, prob=outcome_mat[states[1]])]
+    outcomes[1] <- outcome_label[sample(1:length(outcome_label), 1, prob=outcome_mat[states[1],])]
     # Continue simulating
     for (i in 2:n) {
-        states[i] <- state_label[sample(1:length(state_label), 1, prob=trans_mat[states[i-1]])]
-        outcomes[i] <- outcome_label[sample(1:length(outcome_label), 1, prob=outcome_mat[states[i]])]
+        states[i] <- state_label[sample(1:length(state_label), 1, prob=trans_mat[states[i-1],])]
+        outcomes[i] <- outcome_label[sample(1:length(outcome_label), 1, prob=outcome_mat[states[i],])]
     }
     return(list(states, outcomes))
 }
